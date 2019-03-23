2892acb60e4cf2f895ef29f2389f338f781a1bc4
Balb1 <sami_balbinot@hotmail.com>
2019-03-22 21:16:01 -0300

    Tela trocar senha criada

---
+++ src/app/tela-trocar-senha-matricula/tela-trocar-senha-matricula.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-trocar-senha-matricula',
+  templateUrl: './tela-trocar-senha-matricula.page.html',
+  styleUrls: ['./tela-trocar-senha-matricula.page.scss'],
+})
+export class TelaTrocarSenhaMatriculaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
